4c45517e651713c440cf9399d5df34c9684d8264
TomasMarina <tomasimarina@gmail.com>
2023-05-01 08:51:25 +0200

    Update

---
+++ RunLIM.R
@@ -0,0 +1,15 @@
+require(LIM)
+
+# Read input file and setup the LIM
+readLIM <- Read("flowbeagle.lim")
+readLIM
+
+LIMBeagle <- Setup(readLIM)
+LIMBeagle
+
+# Solve the LIM by range estimation
+flowranges <- Xranges(LIMBeagle)
+Plotranges(LIMBeagle, lab.cex = 0.7, xlab = "g C /m^2/d",
+           main = "Beagle Channel food web")
+
+
