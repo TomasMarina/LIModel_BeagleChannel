7094afeff3a5adfab9502d8a26f6dd11b4a73d12
TomasMarina <tomasimarina@gmail.com>
2023-05-03 13:01:06 +0200

    Circular plot of food web

--- RunLIM.R
+++ RunLIM.R
@@ -12,4 +12,7 @@ flowranges <- Xranges(LIMBeagle)
 Plotranges(LIMBeagle, lab.cex = 0.7, xlab = "g C /m^2/d",
            main = "Beagle Channel food web")
 
+# Plot a single solution
+plotweb(Flowmatrix(LIMBeagle),
+        main="Beagle food web", bty="n", lab.size=0.85)
 
